0031c4dddbb2909b27de0a2a00a2c812e322c98a
Namaniel <nathaniel.nguyen@unc.edu>
2023-05-08 01:14:07 -0400

    Added comments to the rpsls-cli.js file to improve readability.

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 import minimist from 'minimist';
-import {rpsls} from "../lib/rpsls.js"
+import {rpsls} from "../lib/rpsls.js";
 
+// Parse command-line arguments
 var arg = minimist(process.argv.slice(2));
 
+// Help message with instructions and usage examples
 const help_message = `Usage: node-rpsls [SHOT]
 Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
   -h, --help        display this help message and exit
@@ -15,6 +17,7 @@ Examples:
                     e.g {"player":"rock","opponent":"Spock","result":"lose"}
 `;
 
+// Rules for the Lizard-Spock Expansion of Rock Paper Scissors
 const rules = `Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
 - Scissors CUTS Paper
 - Paper COVERS Rock
@@ -28,20 +31,22 @@ const rules = `Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
 - Rock CRUSHES Scissors
 `;
 
-if (arg.h || arg.rules) {
-        console.log(help_message);
-        process.exit(0);
+// Display help message if -h or --help flag is provided
+if (arg.h || arg.help) {
+    console.log(help_message);
+    process.exit(0);
 }
 
+// Display rules if -r or --rules flag is provided
 if (arg.r || arg.rules) {
-        console.log(rules);
-        process.exit(0);
+    console.log(rules);
+    process.exit(0);
 }
 
-let shot = arg._[0]
+let shot = arg._[0]; // Get the user's shot from command line arguments
 
 try {
-        console.log(JSON.stringify(rpsls(shot)));
+    console.log(JSON.stringify(rpsls(shot))); // Play RPSLS and display the result
 } catch (e) {
-        console.log(rules);
+    console.log(rules); // Display rules if an error occurs
 }
